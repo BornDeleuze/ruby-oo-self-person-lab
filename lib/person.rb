080d2909ab803220530b8608d80eb775146f4ea0
Matt Beaudoin <ehxtwo@gmail.com>
2021-03-02 16:24:16 -0800

    commit tree 2ff7f69d6f2a9aac51d6f4b06627202b9691741f

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,66 @@
-# your code goes here
+require 'pry'
+class Person
+    attr_reader :name
+    attr_accessor :bank_account
+    attr_writer :happiness, :hygiene
+    def happiness
+        @happiness.clamp(0, 10)  
+    end
+    def hygiene
+        @hygiene.clamp(0, 10)
+    end
+    friends = []
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+        # friends << self
+    end
+    def clean?
+        @hygiene > 7
+    end
+    def happy?
+        @happiness > 7
+    end
+    def get_paid(salary)
+        @bank_account += salary
+        "all about the benjamins"
+    end
+    def take_bath
+        self.hygiene= @hygiene + 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+    def work_out
+        self.happiness= @happiness + 2
+        self.hygiene= @hygiene - 3
+        "♪ another one bites the dust ♫"
+    end
+
+
+
+
+    def call_friend(person)
+        self.happiness+= 3
+        person.happiness += 3
+        # binding.pry
+        "Hi #{person.name}! It's #{self.name}. How are you?"
+    
+    end
+    def start_conversation(person, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            person.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            person.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+
+    end
+
+end
